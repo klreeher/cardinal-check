ac9260665e0c9cea3933f90669cccb817f10f149
Kate Reeher <kreeher@korterra.com>
2021-12-10 18:08:26 -0600

    configuration files!!!

--- framework-core/Configuration/Configuration.cs
+++ framework-core/Configuration/Configuration.cs
@@ -1,57 +1,92 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.IO;
+using framework_core.Logging;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.CommandLine;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace framework_core
-{
+    {
     /// <summary>
     ///  An instance that provides access to json config values.
     /// </summary>
     public sealed class ConfigurationService
-    {
+        {
         private static ConfigurationService instance;
-
-        private ConfigurationService()
-        {
-            var builder = new ConfigurationBuilder().AddJsonFile(path: GetConfigFile(), false, true);
-
-            this.Root = builder.Build();
-        }
-
         /// <summary>
-        /// Gets a singular instance of access to config values.
+        /// 
         /// </summary>
-        public static ConfigurationService Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new ConfigurationService();
-                }
-
-                return instance;
-            }
-        }
-
+        private ConfigurationBuilder _builder;
         /// <summary>
         /// Gets the root object for the configuration file.
         /// </summary>
-        public IConfigurationRoot Root { get; }
+        public IConfigurationRoot Root { get; set; }
 
-        private static string GetConfigFile()
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ConfigurationService()
+            {
+            this._builder = new ConfigurationBuilder();
+            this._builder.AddEnvironmentVariables();
+            }
+        public void buildConfigSources()
         {
-#if DEV
-            return "FrameworkSettings.dev.json";
-#endif
-#if QA
-            return "BaseFrameworkSettings.qa.json";
-#endif
-#if PROD
-            return "FrameworkSettings.prod.json";
-#endif
-            throw new FileNotFoundException(
-                "No configuration file found for the currently configured build configuration.");
+            this.Root = this._builder.Build();
+        }
+
+        public string getSection(string sectionName)
+        {
+            return this.Root.GetSection(sectionName).Value;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void addJsonConfig(string filePath) => this._builder.AddJsonFile(path: filePath, false, true);
+        /// <summary>
+        /// 
+        /// </summary>
+        public void listSources()
+            {
+            int i = 0;
+            while (i <= this._builder.Sources.Count-1){
+                    if(this._builder.Sources[i].GetType().Name == "JsonConfigurationSource")
+                {
+                    JsonConfigurationSource test = (JsonConfigurationSource)this._builder.Sources[i];
+                    Logger.Info($"{i} type: {test.GetType()} path: {test.Path}");
+                }
+                    else if(this._builder.Sources[i].GetType().Name == "CommandLineConfigurationExtensions"){
+                    CommandLineConfigurationSource cmd = (CommandLineConfigurationSource)this._builder.Sources[i];
+                    Logger.Info($"{i} type: {cmd.GetType()} path: {cmd.Args}");
+                }
+                    else
+                {
+                    Logger.Info($"{i} type: {this._builder.Sources[i].GetType().Name}");
+                }
+
+                i++;
+                }
+            }
+
+        /// <summary>
+        /// Gets a singular instance of access to config values.
+        /// instantiates the ConfigurationService with a configuration builder with only env var.
+        /// </summary>
+        public static ConfigurationService Instance
+            {
+            get
+                {
+                if (instance == null)
+                    {
+                    instance = new ConfigurationService();
+                    }
+
+                return instance;
+                }
+            }
         }
     }
-}
--- tests-unit/UnitTest1.cs
+++ tests-unit/UnitTest1.cs
@@ -1,19 +1,46 @@
 using NUnit.Framework;
-using framework_web;
+using framework_core;
+using System.IO;
+using framework_core.Logging;
 
 namespace tests_unit
 {
-    public class Tests : WebTestBase
+    public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        public ConfigurationService Config;
+
+        [OneTimeSetUp]
+        public void SetUp()
+            {
+            string file = "appSettings.json";
+            Logger.Info("Full Path of Config File:"+Path.GetFullPath(file));
+            this.Config = new ConfigurationService();
+            this.Config.addJsonConfig(Path.GetFullPath(file));
+            
+            }
+        [Test]
+        public void ListSources()
+            {
+            this.Config.listSources();
+            }
 
         [Test]
-        public void Test1()
+        public void WhatIsIt()
         {
-            Assert.Pass();
+            this.Config.buildConfigSources();
+            var allThese = this.Config.Root.GetChildren().GetEnumerator();
+            while( allThese.MoveNext())
+            {
+               // Logger.Info(allThese.Current.Key);
+            }
+
+            var timeSetting = this.Config.Root.GetSection("timeoutSettings").GetChildren().GetEnumerator();
+            while (timeSetting.MoveNext())
+            {
+                Logger.Info(timeSetting.Current.Key);
+            }
+
         }
     }
 }
+
