03653c40ee77f0361ca93265f1ffdde7c3526e66
Kate Reeher <kreeher@korterra.com>
2021-12-13 15:24:19 -0600

    logger & dotnet build

--- framework-core/Logging/Logger.cs
+++ framework-core/Logging/Logger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Diagnostics;
 
 namespace framework_core.Logging
@@ -7,47 +8,45 @@ namespace framework_core.Logging
     /// A custom logging class for CardinalCheck tests.
     /// </summary>
     public class Logger
+    {
+        /// <summary>
+        /// Write an info-level loging message to the console.
+        /// </summary>
+        /// <param name="msg">The string message to be written to the log.</param>
+        public static void Info(string msg)
         {
-            /// <summary>
-            /// Write an info-level loging message to the console.
-            /// </summary>
-            /// <param name="msg">The string message to be written to the log.</param>
-            public static void Info(string msg)
-            {
-                Console.WriteLine(msg);
-                Debug.WriteLine(msg);
-            }
+            Info(msg: msg, args: Array.Empty<string>());
+        }
 
-            /// <summary>
-            /// Write an error-level loging message to the console.
-            /// </summary>
-            /// <param name="msg">The string message to be written to the log.</param>
-            public static void Error(string msg)
-            {
-                Console.Error.WriteLine(msg);
-                Debug.WriteLine(msg);
-            }
+        /// <summary>
+        /// Write an error-level loging message to the console.
+        /// </summary>
+        /// <param name="msg">The string message to be written to the log.</param>
+        public static void Error(string msg)
+        {
+            Error(msg: msg, args: Array.Empty<string>());
+        }
 
-            /// <summary>
-            /// Write an info-level loging message to the console.
-            /// </summary>
-            /// <param name="msg">The string message to be written to the log.</param>
-            /// <param name="args">An array of strings to be appended to the message.</param>
-            public static void Info(string msg, params string[] args)
-            {
-                Console.WriteLine(msg, args);
-                Debug.WriteLine(msg, args);
-            }
+        /// <summary>
+        /// Write an info-level loging message to the console.
+        /// </summary>
+        /// <param name="msg">The string message to be written to the log.</param>
+        /// <param name="args">An array of strings to be appended to the message.</param>
+        public static void Info(string msg, params string[] args)
+        {
+            TestContext.WriteLine(msg, args);
+            Debug.WriteLine(msg, args);
+        }
 
-            /// <summary>
-            /// Write an error-level loging message to the console.
-            /// </summary>
-            /// <param name="msg">The string message to be written to the log.</param>
-            /// <param name="args">An array of strings to be appended to the message.</param>
-            public static void Error(string msg, params string[] args)
-            {
-                Console.Error.WriteLine(msg, args);
-                Debug.WriteLine(msg, args);
-            }
+        /// <summary>
+        /// Write an error-level loging message to the console.
+        /// </summary>
+        /// <param name="msg">The string message to be written to the log.</param>
+        /// <param name="args">An array of strings to be appended to the message.</param>
+        public static void Error(string msg, params string[] args)
+        {
+            TestContext.WriteLine(msg, args);
+            Debug.WriteLine(msg, args);
         }
     }
+}
